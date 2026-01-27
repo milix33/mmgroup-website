87b80359b6291cb7b425981f97bb09bbead1ffea
milix33 <milix33@github.com>
2026-01-27 16:50:18 +0100

    work

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,7 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://mmgroup.ba',
+  redirects: {
+    '/work': '/portfolio',
+  },
 });
